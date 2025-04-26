eedd690e8efb9a2f3e4c64dea554f7b2f98ce076
Lola Castan <lcastan.pro@gmail.com>
2025-04-26 12:43:47 +0200

    Ajout de la migration pour la création de la table 'event' et implémentation de l'entité Event avec ses propriétés et méthodes associées, ainsi que la mise en place du repository EventRepository.

---
+++ src/Entity/Event.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $shortDescription = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $longDescription = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $location = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startDateTime = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $endDateTime = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->shortDescription;
+    }
+
+    public function setShortDescription(?string $shortDescription): static
+    {
+        $this->shortDescription = $shortDescription;
+
+        return $this;
+    }
+
+    public function getLongDescription(): ?string
+    {
+        return $this->longDescription;
+    }
+
+    public function setLongDescription(string $longDescription): static
+    {
+        $this->longDescription = $longDescription;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?string $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getStartDateTime(): ?\DateTimeImmutable
+    {
+        return $this->startDateTime;
+    }
+
+    public function setStartDateTime(\DateTimeImmutable $startDateTime): static
+    {
+        $this->startDateTime = $startDateTime;
+
+        return $this;
+    }
+
+    public function getEndDateTime(): ?\DateTimeImmutable
+    {
+        return $this->endDateTime;
+    }
+
+    public function setEndDateTime(\DateTimeImmutable $endDateTime): static
+    {
+        $this->endDateTime = $endDateTime;
+
+        return $this;
+    }
+}
